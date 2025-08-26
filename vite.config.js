25b000bb75881ccba717d861688a9c73f65ac350
Beans Kuhlman <bkuhlman80@gmail.com>
2025-08-26 11:20:49 -0600

    vite build -> docs for Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  // GitHub Pages base path for bkuhlman80.github.io/helio-geo-zodiac/
   base: '/helio-geo-zodiac/',
   build: {
-    outDir: 'docs',     // Pages can publish from /docs on main
-    assetsDir: '',      // keep files flat (nice for Pages)
+    outDir: 'docs',
+    assetsDir: '',
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'index.html'),
